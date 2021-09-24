7c6cd812572393d4ed8e5fa0357c3b38eb9832e3
veronicahwambui <njengaveronicahwambui@gmail.com>
2021-09-24 15:44:55 +0300

    deploy

--- schoolsystem/schoolsystem/settings.py
+++ schoolsystem/schoolsystem/settings.py
@@ -90,12 +90,16 @@ WSGI_APPLICATION = 'schoolsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'schoolsystem',
+        'USER' : 'njengavero',
+        'PASSWORD': 'njenga3881',
+        'HOST': 'localhost',
+        'PORT':''
+
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
